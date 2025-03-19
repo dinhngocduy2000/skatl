a07e6e9c1c281867de09ffc8080c27908339d414
Dylan Dinh <dylan.dinh@techvify.com.vn>
2025-03-20 00:04:11 +0700

    feat: shadcn layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -2,6 +2,21 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "../globals.css";
 import { defaultMetadata } from "@/lib/utils";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,15 +39,43 @@ export default function AuthenticatedLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className="box-border flex h-screen w-screen justify-center">
-          <section className="my-auto box-border flex h-[calc(100vh_-_16px)] w-[calc(100vw_-_16px)] gap-2">
-            {/* <SidebarComponent /> */}
-            <main className="flex h-full flex-1 flex-col overflow-auto rounded-lg bg-default">
-              <div className="block">{children}</div>
-              {/* <FooterComponent /> */}
-            </main>
-          </section>
-        </main>
+        <SidebarProvider>
+          <main className="box-border flex h-screen w-screen justify-center">
+            <section className="my-auto box-border flex h-full w-full gap-2">
+              <AppSidebar />
+              <SidebarInset>
+                <main className="flex h-full flex-1 flex-col overflow-auto rounded-lg bg-default">
+                  <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                      <SidebarTrigger className="-ml-1" />
+                      <Separator
+                        orientation="vertical"
+                        className="mr-2 data-[orientation=vertical]:h-4"
+                      />
+                      <Breadcrumb>
+                        <BreadcrumbList>
+                          <BreadcrumbItem className="hidden md:block">
+                            <BreadcrumbLink href="#">
+                              Building Your Application
+                            </BreadcrumbLink>
+                          </BreadcrumbItem>
+                          <BreadcrumbSeparator className="hidden md:block" />
+                          <BreadcrumbItem>
+                            <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                          </BreadcrumbItem>
+                        </BreadcrumbList>
+                      </Breadcrumb>
+                    </div>
+                  </header>
+                  <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                    {children}
+                  </div>
+                  {/* <FooterComponent /> */}
+                </main>
+              </SidebarInset>
+            </section>
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
