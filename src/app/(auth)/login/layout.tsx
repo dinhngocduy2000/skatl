5df9688e80d2baed451289a1eb21213561d7e175
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-04-26 23:37:28 +0700

    fix: add missing css

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,6 +1,7 @@
 import { DefaultMetadata } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
+import "../../globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
