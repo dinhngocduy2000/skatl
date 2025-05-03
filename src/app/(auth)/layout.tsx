58779b78dc70e99473128e86d26a406e03103dfe
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-05-03 21:49:27 +0700

    fix: fix fonts

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import { DefaultMetadata } from "@/lib/utils";
 import ToastProvider from "../toast-provider";
 import Logo from "../../assets/svg/skatl-logo.svg";
+import { Geist, Geist_Mono } from "next/font/google";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
+
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
-
 export const metadata: Metadata = DefaultMetadata;
 
 export default function LoginLayout({
