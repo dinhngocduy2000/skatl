43665b2cb257749983406095c7b6af202545a283
Dylan Dinh <ngocduydinh2000@gmail.com>
2025-05-04 16:48:50 +0700

    fix: fix build error

--- src/app/(private)/(home)/client-component.tsx
+++ src/app/(private)/(home)/client-component.tsx
@@ -3,9 +3,7 @@ import { testAction } from "@/actions/test";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
-type Props = {};
-
-const ClientComponent = (props: Props) => {
+const ClientComponent = () => {
   const { data } = useQuery({
     queryKey: ["test"],
     queryFn: () => testAction(),
